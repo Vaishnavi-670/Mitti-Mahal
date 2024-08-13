3a853573c00185cd59e14921890991bbdb1cf93e
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-08-13 22:46:48 +0530

    .

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    
+      <div className='  h-[100vh] w-[100%] bg-gray-50'>
+        <div className='flex h-screen'>
+          <img className= 'h-1/2 w-1/3 object-cover rounded-e-full' src="https://i.pinimg.com/564x/cf/c4/2b/cfc42b42cd725d9af615d1d95156c26b.jpg" alt="" />
+
+        </div>
+      </div>
     </>
   )
 }
