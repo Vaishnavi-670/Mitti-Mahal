e426834f04b62bd06f9bb9daaff7d5a77836aa0e
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-08-19 20:38:23 +0530

    ..

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Login = () => {
     return (
         <>
-            <div className='flex justify-center    items-center h-screen'>
-                <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+            <div className='flex justify-center items-center h-screen'>
+                <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-gray-400 shadow-md">
                     <div className="relative mx-4 -mt-6 mb-4 grid h-20 place-items-center overflow-hidden rounded-xl bg-gradient-to-tr from-cyan-600 to-cyan-400 bg-clip-border text-white shadow-lg shadow-cyan-500/40">
                         <h3 className="block font-sans text-3xl font-semibold leading-snug tracking-normal text-white antialiased">
                             Login Page
--- frontend/src/app/signup/page.jsx
+++ frontend/src/app/signup/page.jsx
@@ -6,7 +6,7 @@ const Signup = () => {
     return (
         <>
             <div className='flex items-center justify-center h-screen'>
-                <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border shadow-gray-400 text-gray-700 shadow-md">
                     <div className="relative mx-4 -mt-6 mb-4 grid h-20 place-items-center overflow-hidden rounded-xl bg-gradient-to-tr from-cyan-600 to-cyan-400 bg-clip-border text-white shadow-lg shadow-cyan-500/40">
                         <h3 className="block font-sans text-3xl font-semibold leading-snug tracking-normal text-white antialiased">
                             Sign Up
