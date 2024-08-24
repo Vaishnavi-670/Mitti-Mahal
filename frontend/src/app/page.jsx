d215dd8910b04f5cd302b1579e0c3a46f25bd4f4
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-08-24 23:41:15 +0530

    ./

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useEffect, useRef } from 'react';
 import LocomotiveScroll from 'locomotive-scroll';
 import 'locomotive-scroll/dist/locomotive-scroll.css';
+import Link from 'next/link';
 
 const Home = () => {
   const scrollRef = useRef(null);
@@ -17,7 +18,7 @@ const Home = () => {
       if (scroll) scroll.destroy();
     };
   }, []);
-  return (
+  return ( 
     <div ref={scrollRef}>
       <section className="pt-24 bg-white">
         <div className=" mx-auto max-w-7xl">
@@ -33,7 +34,7 @@ const Home = () => {
               Unleash your creativity with our premium clay products. Perfect for artists of all levels, our clay is designed to help you bring your visions to life.
             </p>
             <div className=" space-x-0 md:space-x-2 ">
-              <a
+              <Link
                 href="#_"
                 className="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg text-white bg-red-400 rounded-2xl sm:w-auto sm:mb-0"
               >
@@ -50,8 +51,8 @@ const Home = () => {
                     clipRule="evenodd"
                   />
                 </svg>
-              </a>
-              <a
+              </Link>
+              <Link
                 href="/learnmore"
                 className="inline-flex items-center justify-center w-full px-6 py-3  text-lg bg-gray-100 rounded-2xl sm:w-auto sm:mb-0"
               >
@@ -70,7 +71,7 @@ const Home = () => {
                     d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
                   />
                 </svg>
-              </a>
+              </Link>
             </div>
           </div>
         </div>
