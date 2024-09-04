62f203f584a61cce2bede4ea3ecd21df898ccb01
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-09-04 21:50:39 +0530

    .

--- backend/connection.js
+++ backend/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const url = "mongodb+srv://vaishnavi357:vaishnavi123@cluster0.k4yrt8l.mongodb.net/mittimahal?retryWrites=true&w=majority&appName=Cluster0"
 
-//connect to database 
 
+//connect to database 
 mongoose.connect(url) 
 .then((result) => {
     console.log('connected to database');
