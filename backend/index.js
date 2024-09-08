66a92c1f0a6c87e904f738947652e9863efecdfe
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-09-08 17:59:25 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 const port =5000;
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: '*'
 }));  // Enable cross-origin resource sharing (CORS) for all requests
 app.use(express.json()); 
 app.use('/user', userRouter);
