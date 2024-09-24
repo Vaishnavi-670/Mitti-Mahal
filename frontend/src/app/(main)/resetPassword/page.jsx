6d5f725f4842a5321d0f70a3459169bdadf59c6f
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-09-24 22:56:25 +0530

    ,

---
+++ frontend/src/app/(main)/resetPassword/page.jsx
@@ -0,0 +1,72 @@
+'use client'
+import React, { useState } from 'react';
+
+const ResetPassword = () => {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  const handlePasswordReset = (e) => {
+    e.preventDefault();
+    if (password === confirmPassword) {
+      setSuccess('Your password has been successfully reset.');
+      setError('');
+    } else {
+      setError('Passwords do not match.');
+      setSuccess('');
+    }
+  };
+
+  return (
+    <div className="min-h-screen  flex flex-col justify-center items-center">
+      <div className="bg-gray-100 shadow-md rounded-lg p-8 w-full max-w-md">
+        <h2 className="text-2xl font-bold text-center text-gray-700 mb-6">
+          Reset Password
+        </h2>
+        <form onSubmit={handlePasswordReset}>
+          <div className="mb-4">
+            <label className="block text-gray-600 text-sm font-semibold mb-2" htmlFor="password">
+              New Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-3 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-orange-400"
+              placeholder="Enter new password"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-600 text-sm font-semibold mb-2" htmlFor="confirmPassword">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="w-full px-3 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-orange-400"
+              placeholder="Confirm new password"
+              required
+            />
+          </div>
+          {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
+          {success && <p className="text-green-500 text-sm mb-4">{success}</p>}
+          <div>
+            <button
+              type="submit"
+              className="w-full bg-orange-500 text-white py-2 px-4 rounded-md hover:bg-orange-600 transition duration-200"
+            >
+              Reset Password
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ResetPassword;
