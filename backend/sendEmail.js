c7365ca96d664ec6f18172638e315dd5fb0d8fb0
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-09-24 18:50:16 +0530

    ,,

--- backend/sendEmail.js
+++ backend/sendEmail.js
@@ -1,28 +1,38 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 // Create transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
-  service: 'gmail', // You can also use SMTP details like 'smtp.example.com'
-  auth: {
-    user: 'mittimahal8@gmail.com', // Your email address
-    pass: 'hjmwnyebiewaocsq', // Your email password (or App password if using 2-factor authentication)
-  },
+    service: 'gmail', // You can also use SMTP details like 'smtp.example.com'
+    auth: {
+        user: process.env.EMAIL, // Your email address
+        pass: process.env.EMAIL_PASSWORD, // Your email password (or App password if using 2-factor authentication)
+    },
 });
+
+function generateOTP() {
+    // Generate a random number between 100000 and 999999
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    return otp.toString(); // Convert to string if needed
+}
+
+// Example usage
+const otp = generateOTP();
+console.log('Generated OTP:', otp);
 
 // Define mail options
 let mailOptions = {
-  from: 'mittimahal8@gmail.com', // Sender address
-  to: 'recipient-email@example.com', // List of receivers
-  subject: 'Hello from Nodemailer', // Subject line
-  text: 'Hello! This is a test email sent using Nodemailer.', // Plain text body
-  html: '<b>Hello!</b> This is a test email sent using Nodemailer.', // HTML body
+    from: process.env.EMAIL, // Sender address
+    to: 'chauhanvaishnavi357@gmail.com', // List of receivers
+    subject: 'Hello from Nodemailer', // Subject line
+    html:`<b>Your OTP code is: ${otp}</b>`, // HTML body
 };
 
 // Send email with defined transport object
 transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    return console.log(error);
-  }
-  console.log('Message sent: %s', info.messageId);
-  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 });
