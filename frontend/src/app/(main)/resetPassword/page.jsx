d003c420ad94aac8a141288d0326419da0cfa11b
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-09-28 17:44:47 +0530

    ..

--- frontend/src/app/(main)/resetPassword/page.jsx
+++ frontend/src/app/(main)/resetPassword/page.jsx
@@ -19,54 +19,63 @@ const ResetPassword = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <form onSubmit={handlePasswordReset}>
-      <div className="bg-gray-100 h-[50vh] shadow-md rounded-lg p-8 w-full max-w-md">
-        <h2 className="text-2xl font-bold text-center text-gray-700 mb-6">
-          Reset Password
-        </h2>
-          <div className="mb-4">
-            <label className="block text-gray-600 text-sm font-semibold mb-2" htmlFor="password">
-              New Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-3 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-orange-400"
-              placeholder="Enter new password"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-600 text-sm font-semibold mb-2" htmlFor="confirmPassword">
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              id="confirmPassword"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className="w-full px-3 py-2 border rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-orange-400"
-              placeholder="Confirm new password"
-              required
-            />
-          </div>
-          {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
-          {success && <p className="text-green-500 text-sm mb-4">{success}</p>}
-          <div>
-            <button
-              type="submit"
-              className="w-full bg-orange-500 text-white py-2 px-4 rounded-md hover:bg-orange-600 transition duration-200"
-            >
-              Reset Password
-            </button>
-          </div>
+    <div className='flex justify-center items-center h-screen'>
+    <form onSubmit={handlePasswordReset}>
+        <div className="relative h-96 flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-gray-400 shadow-md">
+            <div className="relative mx-4 -mt-6 mb-4 grid h-20 place-items-center overflow-hidden rounded-xl bg-gradient-to-tr from-cyan-600 to-cyan-400 bg-clip-border text-white shadow-lg shadow-cyan-500/40">
+                <h3 className="block font-sans text-3xl font-semibold leading-snug tracking-normal text-white antialiased">
+                    ResetPassword
+                </h3>
+            </div>
+            <div className="flex flex-col gap-4 p-6">
+                <div className="relative h-16 mt-1 w-full min-w-[200px]">
+                    <input id='password' onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                        placeholder=""
+                        className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-cyan-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                    />
+                    <label className="before:content[' '] py-3 after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] 
+                    after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md 
+                    after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] 
+                    peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-cyan-500 peer-focus:before:border-t-2 
+                    peer-focus:before:border-l-2  peer-focus:before:!border-cyan-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-cyan-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent 
+                    peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                        New Password
+                    </label>
+                </div>
+                <div className="relative mt-3 h-16 w-full min-w-[200px]">
+                    <input type='password' id='confirmpassword' onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword}
+                        placeholder=""
+                        className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-cyan-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                    />
+                    <label className="before:content[' '] py-3 after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px]
+                     before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5
+                      after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent
+                       peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-cyan-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-cyan-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2
+                        peer-focus:after:!border-cyan-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                       Confirm Password
+                    </label>
+                </div>
+                
+            </div>
+            <div className="p-6 pt-3">
+                <button
+                    data-ripple-light="true"
+                    type="submit"
+                    className="block w-full select-none rounded-lg bg-gradient-to-tr from-cyan-600 to-cyan-400 py-5 px-6 text-center align-middle font-sans
+                     text-xs font-bold uppercase text-white shadow-md shadow-cyan-500/20 transition-all hover:shadow-lg hover:shadow-cyan-500/40 active:opacity-[0.85] disabled:pointer-events-none
+                      disabled:opacity-50 disabled:shadow-none"
+                >
+                    Submit
+                </button>
 
-      </div>
-      </form>
-    </div>
+                
+            </div>
+        </div>
+    </form>
+
+</div>
+
   );
 };
 
