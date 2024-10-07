e9046b3e13a1a31f187a44afdb547ffe9795ad49
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-10-07 17:41:58 +0530

    ..

--- frontend/src/app/admin/manageproduct/page.jsx
+++ frontend/src/app/admin/manageproduct/page.jsx
@@ -23,26 +23,15 @@ const ManageProduct = () => {
       fetchProduct();
     }
   }
-  const displayProductData = () => {
-    return productList.map(product => (
-      <tr key={product._id}>
-        <td>{product.title}</td>
-        <td>{product.category}</td>
-        <td>{product.description}</td>
-        <td>{product.price}</td>
-        <td>{product.image}</td>
-        <td><button onClick={() => deleteUser(product._id)}>Delete</button></td>
-      </tr>
-    ))
-  }
+  
   return (
-    <>
+    
       <>
         {/* Table Section */}
-        <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <div className=" px-4 py-5 mt-0 sm:px-6 lg:px-8 lg:py-14 mx-auto">
           {/* Card */}
           <div className="flex flex-col">
-            <div className="-m-1.5 overflow-x-auto">
+            <div className=" overflow-x-auto">
               <div className="p-1.5 min-w-full inline-block align-middle">
                 <div className="bg-white border border-gray-200 rounded-xl shadow-sm overflow-hidden dark:bg-neutral-900 dark:border-neutral-700">
                   {/* Header */}
@@ -564,7 +553,7 @@ const ManageProduct = () => {
         {/* End Table Section */}
       </>
 
-    </>
+  
   )
 }
 
