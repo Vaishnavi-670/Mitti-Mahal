d804da7ce8331a95a13fe08dec4c44baa8700fe6
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2024-10-14 22:43:29 +0530

    ,,

--- frontend/src/app/(main)/contact/page.jsx
+++ frontend/src/app/(main)/contact/page.jsx
@@ -130,14 +130,13 @@ const Contact = () => {
                     </div>
                 </div>
 
-
                 <div className="h-96 bg-gray-300 rounded-lg overflow-hidden shadow-lg">
                     <iframe
                         title="Mitti Mahal Location"
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.8354345094456!2d144.95373531576016!3d-37.81627974257174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d43f1f7a66b%3A0x4e77a30f76544a76!2sMitti%20Mahal!5e0!3m2!1sen!2sau!4v1630075416933!5m2!1sen!2sau"
                         className="w-full h-full"
                         allowFullScreen=""
-                        loading="lazy"
+                        loading="eagers"
                     ></iframe>
                 </div>
             </div>
