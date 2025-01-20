bb635f1bf869e77ea7902b3e90ff6959a773582a
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2025-01-20 10:50:45 +0530

    m

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import PrelineScript from "@/Components/PrelineScript";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +15,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}
+
+
         <Toaster position="top-center" />
-        <PrelineScript/>
+        <PrelineScript />
+
+        <Script strategy="lazyOnload">
+          {
+            `var Tawk_API=Tawk_API||{ }, Tawk_LoadStart=new Date();
+          (function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+          s1.async=true;
+          s1.src='https://embed.tawk.to/678dd5963a8427326071750d/1ii10klps';
+          s1.charset='UTF-8';
+          s1.setAttribute('crossorigin','*');
+          s0.parentNode.insertBefore(s1,s0);
+})();`
+          }
+        </Script>
       </body>
     </html>
   );
