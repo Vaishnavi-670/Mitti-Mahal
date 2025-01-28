f96a3af74f8a3936a3a4dcc8bbbf7584ae3cecc2
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2025-01-28 22:16:12 +0530

    m

--- frontend/src/app/user/checkout/page.jsx
+++ frontend/src/app/user/checkout/page.jsx
@@ -16,7 +16,6 @@ const CheckoutPage = () => {
             
         }
     }
-
     return (
         <div className="min-h-screen bg-gray-50 py-12">
             <div className="container mx-auto ">
