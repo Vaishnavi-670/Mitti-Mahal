69af5e0967433d5858067cd59bea644540eea1be
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2025-02-13 23:50:19 +0630

    .

--- frontend/src/context/AdminContext.jsx
+++ frontend/src/context/AdminContext.jsx
@@ -17,7 +17,6 @@ export const AdminProvider = ({ children }) => {
   });
 
  
-
   const adminLogout = () => {
     document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
     !ISSERVER ? localStorage.removeItem('admin') :'';
