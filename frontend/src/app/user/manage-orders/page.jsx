f97ac4462b33ba4b9cc01c452abb57256faf443a
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2025-03-14 23:59:10 +0630

    .

--- frontend/src/app/user/manage-orders/page.jsx
+++ frontend/src/app/user/manage-orders/page.jsx
@@ -75,7 +75,7 @@ const ManageOrdersPage = () => {
                       /> */}
                       {order._id}
                     </td>
-                    
+               
                     <td className="px-4 py-2 border">{order.items.title}</td>
                     <td className="px-4 py-2 border">{order.items.length}</td>
                     <td className="px-4 py-2 border">₹{calculateItemsTotal(order.items)}</td>
