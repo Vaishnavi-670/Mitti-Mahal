716c13ddaf74020fba51d8b481fd475822b75b56
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2025-03-14 23:57:47 +0630

    .

--- frontend/src/app/user/manage-orders/page.jsx
+++ frontend/src/app/user/manage-orders/page.jsx
@@ -55,7 +55,6 @@ const ManageOrdersPage = () => {
             <table className="min-w-full bg-white border border-gray-200 shadow-md rounded-lg">
               <thead className="bg-gray-200">
                 <tr>
-
                   <th className="px-4 py-2 border">Product Id</th>
                   <th className="px-4 py-2 border">Product Name</th>
                   <th className="px-4 py-2 border">Price</th>
