4612957bbb3d1e6e958dc606ea47f3b285972684
Vaishnavi-670 <chauhanvaishnavi684@gmail.com>
2025-03-22 23:38:30 +0630

    .

--- frontend/src/app/(main)/thankyou/page.jsx
+++ frontend/src/app/(main)/thankyou/page.jsx
@@ -38,7 +38,7 @@ const ThankYouContent = () => {
         }),
       }
     );
-    
+   
     console.log(response.status);
     if (response.status === 200) {
       !ISSERVER && sessionStorage.removeItem("bookingDetails");
